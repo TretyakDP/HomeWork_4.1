d488b9c55b0590206499605e209b07df930c86fa
DmitriyTR <skate1233@mail.ru>
2023-11-01 21:40:20 +0300

    Добавьте файлы проекта.

---
+++ Dz4(1)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер массива (больше 2): ");
+        if (int.TryParse(Console.ReadLine(), out int arraySize) && arraySize > 2)
+        {
+            int[] array = new int[arraySize];
+
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите элемент {i + 1}: ");
+                if (int.TryParse(Console.ReadLine(), out int element))
+                {
+                    array[i] = element;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+                    i--;
+                }
+            }
+
+            int sum = 0;
+            foreach (int element in array)
+            {
+                sum += element;
+            }
+
+            Console.WriteLine($"Сумма всех элементов: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный размер массива. Введите число больше 2.");
+        }
+    }
+}
